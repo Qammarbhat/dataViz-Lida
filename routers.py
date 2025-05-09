7dc1870b16ceae23b34aae4a7c970877829dc479
Qammar Bhat <qammaruzzamman@gmail.com>
2025-05-09 11:19:13 +0530

    cors_fix

--- routers.py
+++ routers.py
@@ -390,4 +390,4 @@ async def visualize_from_query(query: str = Query(..., description="Query string
 
 # Entry point
 if __name__ == "__main__":
-    uvicorn.run("routers:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
